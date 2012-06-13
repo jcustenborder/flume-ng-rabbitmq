fba0728ec7c9b8d3957fd6b10edc5108f4bb49b8
Jeremy Custenborder <jcustenborder@gmail.com>
2012-06-13 13:53:54 -0500

    Added license

--- src/main/java/org/apache/flume/RabbitMQUtils.java
+++ src/main/java/org/apache/flume/RabbitMQUtils.java
@@ -1,29 +1,35 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
+
 package org.apache.flume;
 
 import com.google.common.base.Preconditions;
 import com.rabbitmq.client.BasicProperties;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
-import java.io.IOException;
-import java.net.InetAddress;
-import java.net.Socket;
-import java.net.UnknownHostException;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-import javax.net.SocketFactory;
-import javax.net.ssl.SSLSocketFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- *
- * @author jcustenborder
- */
+
 public class RabbitMQUtils {
     private static final Logger log = LoggerFactory.getLogger(RabbitMQUtils.class);
     static final String PREFIX="RabbitMQ";
--- src/main/java/org/apache/flume/sink/rabbitmq/RabbitMQSink.java
+++ src/main/java/org/apache/flume/sink/rabbitmq/RabbitMQSink.java
@@ -1,7 +1,22 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
+
 package org.apache.flume.sink.rabbitmq;
 
 import com.rabbitmq.client.Channel;
@@ -13,10 +28,6 @@ import org.apache.flume.sink.AbstractSink;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- *
- * @author jcustenborder
- */
 public class RabbitMQSink extends AbstractSink implements Configurable {
     private static final Logger log = LoggerFactory.getLogger(RabbitMQSink.class);
     private CounterGroup _CounterGroup;
